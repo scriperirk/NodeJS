8c3910ad69804c596bbc0b373256c93e1c343b35
Scriper.irk <scriper.irk@gmail.com>
2022-08-30 23:23:02 +0800

    Proba10

--- test.js
+++ test.js
@@ -2,11 +2,8 @@ const foo = 1;
 console.log(foo);
 let bar;
 bar = 1;
-function test(
-
-
-) {
-    console.log(baz);
+function test() {
+  console.log(baz);
 }
 // eslint-disable-next-line no-var
 var baz = 123;
